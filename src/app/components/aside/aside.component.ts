4bd98b83daea144b706ff4c6dcb86f149a013da7
Bashar <basharsaeed200@gmail.com>
2023-07-02 23:18:36 +0000

    finish up side bar

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.css']
+  styleUrls: ['./aside.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AsideComponent {
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,15 @@
 
+
+window.onload = () => {
+    const _get = (arg) => {
+        return document.querySelector(arg)
+    }
+    
+
+    if (_get('.toggle-sidebar-btn')) {
+        _get('.toggle-sidebar-btn').addEventListener('click', () => {
+            _get('body').classList.toggle('toggle-sidebar')
+        })
+    }
+    
+}
