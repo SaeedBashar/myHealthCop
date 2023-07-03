ed20e16aacc69450f0e1204f3fa6882112ce0865
Bashar <basharsaeed200@gmail.com>
2023-07-03 23:19:12 +0000

    working on bookings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AsideComponent } from './components/aside/aside.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { UserManagementComponent } from './components/user-management/user-management.component';
 import { MapComponent } from './components/map/map.component';
+import { BookingsComponent } from './components/bookings/bookings.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MapComponent } from './components/map/map.component';
     AsideComponent,
     StatisticsComponent,
     UserManagementComponent,
-    MapComponent
+    MapComponent,
+    BookingsComponent
   ],
   imports: [
     BrowserModule,
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,10 +15,18 @@ window.onload = () => {
     if(_get('#registeredUsersChart')){
         buildRegisteredUsersChart()
     }
-
+    
     if(_get('#demography')){
         buildDemography()
     }
+
+    if(_get('#activeEntitiesChart')){
+        buildActiveEntitiesChart();
+    }
+
+    if(_get('#bookingsChart')){
+      buildBookingsChart()
+  }
 }
 
 const _get = (arg) => {
@@ -433,4 +441,49 @@ const buildDemography = ()=>{
         },
         labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
       }).render();
+}
+
+const buildActiveEntitiesChart = ()=>{
+  new ApexCharts(_get("#activeEntitiesChart"), {
+    series: [44, 55, 13, 43, 22],
+    chart: {
+      height: 350,
+      type: 'pie',
+      toolbar: {
+        show: true
+      }
+    },
+    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E']
+  }).render();
+}
+
+const buildBookingsChart = ()=>{
+  new ApexCharts(document.querySelector("#bookingsChart"), {
+    series: [{
+      name: "Desktops",
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+    chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      },
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+    }
+  }).render();
 }
