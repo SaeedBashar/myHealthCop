3bc18fea8ed189b3bfdc21213666e58e5d433784
Bashar <basharsaeed200@gmail.com>
2023-07-06 23:32:47 +0000

    Finalized

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class MapComponent {
   data:any
-
+  current :any = document.getElementById('path3019')
   constructor(private http: HttpClient){
     http.get("http://localhost:3000/regions")
     .subscribe(res=>{
@@ -18,6 +18,13 @@ export class MapComponent {
   }
 
   setCurrent(arg){
+    let items = document.getElementsByTagName("path")
+    for(let i=0; i<items.length; i++){
+      items[i].style.stroke = '#ffffff';
+      items[i].style.strokeWidth = '3';
+    }
     
+    arg.style.stroke = "red";
+    arg.style.strokeWidth = '5'
   }
 }
